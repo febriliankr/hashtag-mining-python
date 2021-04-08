067ed1d39787d0b2358fb5b2561912545aae1d35
Febrilian Kristiawan <febriliankr@gmail.com>
2021-04-08 14:59:25 +0700

    added dotenv reader and instructions

--- main.py
+++ main.py
@@ -2,18 +2,23 @@ import tweepy
 import csv
 import pandas as pd
 import re
+from dotenv import load_dotenv
 
+load_dotenv()  # take environment variables from .env.
+
+# using Regular Expression (re) to strip out the emojis
 RE_EMOJI = re.compile('[\U00010000-\U0010ffff]', flags=re.UNICODE)
+
 
 def strip_emoji(text):
     return RE_EMOJI.sub(r'', text)
 
-print(strip_emoji('@achilorstuv WKWKWKKWWK KOK BISA SPAGHETTI ADA KRIUKNYA SARAH üò≠🤔'))
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(
     "k1MxRDsdaOJxkAvk4hi1Udxro",
     "TKmTdCg50CS4TXLticIxMc5PVSkm9TsCdSqnCszPOjHuDR4c94")
+    
 auth.set_access_token("321365430-SEEjSZOGgLMsY9hPBdvEZEraQOzMrF5INAh6IzXH",
                       "2B7Yc4fLk9rx2vEmKzrR1V0bXHQq49eUX361ESYebqmL3")
 
@@ -25,30 +30,22 @@ try:
 except:
     print("Error during authentication")
 
-MAX_TWEETS = 10
+# Gather Input
 search_keyword = input("search keyword? ")
-print("searching: '" + search_keyword + "'\n")
-
-# tweets = tweepy.Cursor(api.search,
-#                        q=search_keyword,
-#                        lang="en",
-#                        since="2021-04-04").items(1)
-
-# for tweet in tweets:
-#     print('$' + tweet.text + '\n\n')
-
+search_limit = int(input("search limit amount? "))
+print("searching: '" + search_keyword + " (limit: " + str(search_limit) + ") '\n")
 
 csvFile = open(search_keyword+'.csv', 'a+', newline='', encoding="utf-8")
 
 csvWriter = csv.writer(csvFile)
-search_limit = 10
+
 
 created = []
 id_twitter = []
 username = []
 text = []
 
-for tweet in tweepy.Cursor(api.search, q=search_keyword, count=100, lang='id', result_type="latest").items(search_limit):
+for tweet in tweepy.Cursor(api.search, q=search_keyword, count=300, lang='id', result_type="latest").items(search_limit):
     print(tweet.created_at,  tweet.id, tweet.user.name, tweet.text)
     created.append(tweet.created_at)
     id_twitter.append(tweet.id)
--- readme.md
+++ readme.md
@@ -2,6 +2,20 @@
 
 searches through a string, strips the emoji, and convert it to csv
 
+## Authenticating
+
+```
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+```
+
+`consumer_key` adalah `API Key`, dan `consumer_secret` adalah `API key secret`
+
+```
+auth.set_access_token(key, secret)
+```
+
+`key` adalah `Access token` dan `secret` adalah `Access token secret` yang didapat di portal twitter API
+
 ## Stripping Down The Emoji
 
 ![original tweet](./assets/images/example-1-original-tweet.png)
